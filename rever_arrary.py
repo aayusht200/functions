da9f56cb84fe854817733f2ee42ce16692ff5a16
aayusht200 <93221650+aayusht200@users.noreply.github.com>
2024-03-14 00:49:23 -0400

    Create rever_arrary.py

---
+++ rever_arrary.py
@@ -0,0 +1,13 @@
+def rever_arr(temp):
+    # Get the length of the input array
+    len_arr = len(temp)
+    # Create an empty array to store the reversed elements
+    reverse_arr = np.empty((0,), dtype=int)
+    # Iterate through the input array in reverse order
+    for i in range(len_arr):
+        # Decrement the length of the array in each iteration to access elements in reverse order
+        len_arr = len_arr - 1
+        # Append the element at the current position to the reversed array
+        reverse_arr = np.append(reverse_arr, temp[len_arr])
+    # Return the reversed array
+    return reverse_arr
